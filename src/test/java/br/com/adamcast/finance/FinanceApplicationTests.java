84e4ceeac3db3ca31631deed261fd3fa30d2eb4c
Robert Adam <r.neves1992@gmail.com>
2022-01-20 16:11:15 -0300

    first commit

---
+++ src/test/java/br/com/adamcast/finance/FinanceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.adamcast.finance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinanceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
