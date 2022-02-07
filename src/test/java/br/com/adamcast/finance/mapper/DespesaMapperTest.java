10e76be30ecec71c0365df95a66c316d5ee25d4b
Robert Adam <r.neves1992@gmail.com>
2022-02-07 15:46:34 -0300

    Create unit tests for mappers

---
+++ src/test/java/br/com/adamcast/finance/mapper/DespesaMapperTest.java
@@ -0,0 +1,38 @@
+package br.com.adamcast.finance.mapper;
+
+import br.com.adamcast.finance.model.Despesa;
+import br.com.adamcast.finance.model.dto.DespesaDto;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+public class DespesaMapperTest {
+
+    @Test
+    public void shouldMapDespesaDtoToModel() {
+
+        DespesaDto despesaDto = new DespesaDto("123a", "Teste", new BigDecimal("500.00"), LocalDate.now());
+
+        Despesa despesa = DespesaMapper.INSTANCE.toModel(despesaDto);
+
+        Assertions.assertEquals(despesaDto.getData().toString(), despesa.getData().toString());
+        Assertions.assertEquals(despesaDto.getDescricao(), despesa.getDescricao());
+        Assertions.assertEquals(despesaDto.getDescricao(), despesa.getDescricao());
+
+    }
+
+    @Test
+    public void shouldMapDespesaModelToDespesaDto() {
+
+        Despesa despesa = new Despesa("123a", "Teste", new BigDecimal("500"), LocalDate.now());
+
+        DespesaDto despesaDto = DespesaMapper.INSTANCE.toDto(despesa);
+
+        Assertions.assertEquals(despesa.getData().toString(), despesaDto.getData().toString());
+        Assertions.assertEquals(despesa.getDescricao(), despesaDto.getDescricao());
+        Assertions.assertEquals(despesa.getDescricao(), despesaDto.getDescricao());
+
+    }
+}
--- src/test/java/br/com/adamcast/finance/mapper/ReceitaMapperTest.java
+++ src/test/java/br/com/adamcast/finance/mapper/ReceitaMapperTest.java
@@ -1,4 +1,4 @@
-package br.com.adamcast.finance;
+package br.com.adamcast.finance.mapper;
 
 import br.com.adamcast.finance.mapper.ReceitaMapper;
 import br.com.adamcast.finance.model.Receita;
@@ -14,16 +14,26 @@ public class ReceitaMapperTest {
     @Test
     public void shouldMapReceitaDtoToModel() {
 
-        // Given
         ReceitaDto receitaDto = new ReceitaDto("123a", "Teste", new BigDecimal("500.00"), LocalDate.now());
 
-        // When
         Receita receita = ReceitaMapper.INSTANCE.toModel(receitaDto);
-
-        //
 
         Assertions.assertEquals(receitaDto.getData().toString(), receita.getData().toString());
         Assertions.assertEquals(receitaDto.getDescricao(), receita.getDescricao());
+        Assertions.assertEquals(receitaDto.getDescricao(), receita.getDescricao());
+
+    }
+
+    @Test
+    public void shouldMapReceitaModelToReceitaDto() {
+
+        Receita receita = new Receita("123a", "Teste", new BigDecimal("500"), LocalDate.now());
+
+        ReceitaDto receitaDto = ReceitaMapper.INSTANCE.toDto(receita);
+
+        Assertions.assertEquals(receita.getData().toString(), receitaDto.getData().toString());
+        Assertions.assertEquals(receita.getDescricao(), receitaDto.getDescricao());
+        Assertions.assertEquals(receita.getDescricao(), receitaDto.getDescricao());
 
     }
 }
